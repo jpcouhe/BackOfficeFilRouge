fb9e697d046ef9520e85bbbd3189b923bd3b0317
Jean-philippe COUHE <98466099+jpcouhe@users.noreply.github.com>
2022-12-07 16:49:04 +0100

    CSS

---
+++ src/main/java/com/example/backofficefilrouge/servlet/UserProfilServlet.java
@@ -0,0 +1,28 @@
+package com.example.backofficefilrouge.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "UserProfilServlet", value = "/user/profil")
+public class UserProfilServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String idStr = req.getParameter("id");
+
+
+
+
+        req.getRequestDispatcher("/WEB-INF/user-profil.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
